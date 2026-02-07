6733ab5b6ba92117a23593370a7a637d0ccc4d23
James <jamesmpadula@students.abtech.edu>
2026-02-06 20:51:42 -0500

    First 3 assignments

---
+++ assignments/2-4/2-4 Padula.py
@@ -0,0 +1,7 @@
+#James Padula
+#2/3/2026
+
+name = "Mayson"             
+print(name.upper())
+print(name.lower())
+print(name.title())
---
+++ assignments/2-8/2-8 _code_Padula.py
@@ -0,0 +1,10 @@
+#James Padula
+#2/3/2026
+
+print(6+2) #adds 6 to 2 to get 8
+
+print(10-2) #subtracts 10 from 2 to get 8
+
+print(16/2) #divides 16 from 2 to get 8
+
+print(4 * 2) #multiplies 4 by 2 to get 8
