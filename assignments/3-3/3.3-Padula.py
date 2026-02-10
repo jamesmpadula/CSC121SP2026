aa7f7dfaa98a8b433c5e55867b5e27d90111f0d2
James <jamesmpadula@students.abtech.edu>
2026-02-10 18:19:11 -0500

    Week 3

---
+++ assignments/3-3/3.3-Padula.py
@@ -0,0 +1,11 @@
+#James Padula
+#2/10/2026
+#CSC-121-OA1
+
+trains = ['monorail', 'bullet train', 'funicular', 'maglev train', 'steam train']
+
+print(f"I would like to ride a {trains[0]}.")
+print(f"I would like to ride a {trains[1]}.")
+print(f"I would like to ride a {trains[2]}.")
+print(f"I would like to ride a {trains[3]}.")
+print(f"I would like to ride a {trains[4]}.")
---
+++ assignments/3-4/3-4.py
@@ -0,0 +1,20 @@
+#James Padula
+#2/10/2026
+#CSC-121-OA1
+
+guests = ['Master Chief', 'Frodo Baggins', 'Geico Gecko']
+
+print("Dear " + guests[0] + ", you are invited to dinner at my place.")
+print("Dear " + guests[1] + ", you are invited to dinner at my place.")
+print("Dear " + guests[2] + ", you are invited to dinner at my place.")
+ 
+guests.insert(0, 'Scooby Doo')
+guests.insert(2, 'Patrick Star')
+guests.append('Spongebob Squarepants')
+
+print("I have found a bigger dinner table, so 3 more guests can join us.\n")
+ 
+guests.sort()
+ 
+for guest in guests:
+    print("Dear " + guest + ", you are invited to dinner at my place.")
